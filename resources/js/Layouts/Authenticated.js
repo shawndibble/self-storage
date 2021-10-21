1131ea515fd5484a50b4b0f992a93e6d17529100
Shawn Dibble <shawn.dibble@lexialearning.com>
2021-10-20 22:20:27 -0500

    layout update
    add linking
    fix sidebar
    add favicon
    change icons for nav
    fix head name

--- resources/js/Layouts/Authenticated.js
+++ resources/js/Layouts/Authenticated.js
@@ -4,20 +4,18 @@ import CssBaseline from '@mui/material/CssBaseline';
 import MuiDrawer from '@mui/material/Drawer';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import Box from '@mui/material/Box';
-import MuiAppBar from '@mui/material/AppBar';
+import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import Badge from '@mui/material/Badge';
 import Container from '@mui/material/Container';
 import Grid from "@mui/material/Grid";
 import Link from '@mui/material/Link';
 import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import { mainListItems, secondaryListItems } from './NavItems';
+import NavItems from './NavItems';
+import PageName from './PageName';
 
 function Copyright({ site, ...props }) {
     return (
@@ -33,24 +31,6 @@ function Copyright({ site, ...props }) {
 }
 
 const drawerWidth = 240;
-
-const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    ...(open && {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    }),
-}));
 
 const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
     ({ theme, open }) => ({
@@ -69,10 +49,7 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
                     easing: theme.transitions.easing.sharp,
                     duration: theme.transitions.duration.leavingScreen,
                 }),
-                width: theme.spacing(7),
-                [theme.breakpoints.up('sm')]: {
-                    width: theme.spacing(9),
-                },
+                width: theme.spacing(7)
             }),
         },
     }),
@@ -91,21 +68,15 @@ export default function Authenticated(children) {
         <ThemeProvider theme={mdTheme}>
             <Box sx={{ display: 'flex' }}>
                 <CssBaseline />
-                <AppBar position="absolute" open={open}>
+                <AppBar position="fixed" open={open} sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
                     <Toolbar
-                        sx={{
-                            pr: '24px', // keep right padding when drawer closed
-                        }}
                     >
                         <IconButton
                             edge="start"
                             color="inherit"
                             aria-label="open drawer"
                             onClick={toggleDrawer}
-                            sx={{
-                                marginRight: '36px',
-                                ...(open && { display: 'none' }),
-                            }}
+                            sx={{ marginRight: '10px' }}
                         >
                             <MenuIcon />
                         </IconButton>
@@ -116,7 +87,7 @@ export default function Authenticated(children) {
                             noWrap
                             sx={{ flexGrow: 1 }}
                         >
-                            {site?.name} <ArrowForwardIosIcon />
+                            {site?.name} <ArrowForwardIosIcon /> {PageName(children?.type.name)}
                         </Typography>
                         <IconButton color="inherit">
                             <Badge badgeContent={4} color="secondary">
@@ -126,22 +97,8 @@ export default function Authenticated(children) {
                     </Toolbar>
                 </AppBar>
                 <Drawer variant="permanent" open={open}>
-                    <Toolbar
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'flex-end',
-                            px: [1],
-                        }}
-                    >
-                        <IconButton onClick={toggleDrawer}>
-                            <ChevronLeftIcon />
-                        </IconButton>
-                    </Toolbar>
-                    <Divider />
-                    <List>{mainListItems}</List>
-                    <Divider />
-                    <List>{secondaryListItems}</List>
+                    <Toolbar />
+                    <NavItems />
                 </Drawer>
                 <Box
                     component="main"
@@ -156,11 +113,11 @@ export default function Authenticated(children) {
                     }}
                 >
                     <Toolbar />
-                    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                    <Container maxWidth="lg" sx={{ mt: 3, mb: 3 }}>
                         <Grid container spacing={0}>
                             {children}
                         </Grid>
-                        <Copyright sx={{ pt: 4 }} site={site} />
+                        <Copyright sx={{ pt: 3 }} site={site} />
                     </Container>
 
                 </Box>
--- resources/js/Layouts/NavItems.js
+++ resources/js/Layouts/NavItems.js
@@ -1,70 +1,90 @@
 import * as React from 'react';
-import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import ListSubheader from '@mui/material/ListSubheader';
 import DashboardIcon from '@mui/icons-material/Dashboard';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PaymentsIcon from '@mui/icons-material/Payments';
 import PeopleIcon from '@mui/icons-material/People';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import LayersIcon from '@mui/icons-material/Layers';
-import AssignmentIcon from '@mui/icons-material/Assignment';
+import DomainIcon from '@mui/icons-material/Domain';
+import LogoutIcon from '@mui/icons-material/Logout';
+import SettingsIcon from '@mui/icons-material/Settings';
+import { Inertia } from "@inertiajs/inertia";
+import ListItemButton from "@mui/material/ListItemButton";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
 
-export const mainListItems = (
-    <div>
-        <ListItem button>
-            <ListItemIcon>
-                <DashboardIcon />
-            </ListItemIcon>
-            <ListItemText primary="Dashboard" />
-        </ListItem>
-        <ListItem button>
-            <ListItemIcon>
-                <ShoppingCartIcon />
-            </ListItemIcon>
-            <ListItemText primary="Orders" />
-        </ListItem>
-        <ListItem button>
-            <ListItemIcon>
-                <PeopleIcon />
-            </ListItemIcon>
-            <ListItemText primary="Customers" />
-        </ListItem>
-        <ListItem button>
-            <ListItemIcon>
-                <BarChartIcon />
-            </ListItemIcon>
-            <ListItemText primary="Reports" />
-        </ListItem>
-        <ListItem button>
-            <ListItemIcon>
-                <LayersIcon />
-            </ListItemIcon>
-            <ListItemText primary="Integrations" />
-        </ListItem>
-    </div>
-);
 
-export const secondaryListItems = (
-    <div>
-        <ListSubheader inset>Saved reports</ListSubheader>
-        <ListItem button>
-            <ListItemIcon>
-                <AssignmentIcon />
-            </ListItemIcon>
-            <ListItemText primary="Current month" />
-        </ListItem>
-        <ListItem button>
-            <ListItemIcon>
-                <AssignmentIcon />
-            </ListItemIcon>
-            <ListItemText primary="Last quarter" />
-        </ListItem>
-        <ListItem button>
-            <ListItemIcon>
-                <AssignmentIcon />
-            </ListItemIcon>
-            <ListItemText primary="Year-end sale" />
-        </ListItem>
-    </div>
-);
+export default function NavItems() {
+    const [path, setPath] = React.useState(window.location.pathname)
+
+    const navigateTo = (url) => Inertia.visit(url, {
+        onFinish: visit => setPath(visit?.url?.pathname)
+    })
+
+    return (
+        <div style={{
+            minHeight: 'calc(100vh - 65px)',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between'
+        }}>
+            <List>
+                <ListItemButton
+                    onClick={() => navigateTo('/dashboard')}
+                    selected={path === '/dashboard'}
+                >
+                    <ListItemIcon>
+                        <DashboardIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Dashboard" />
+                </ListItemButton>
+                <ListItemButton
+                    onClick={() => navigateTo('/payment')}
+                    selected={path === '/payment'}
+                >
+                    <ListItemIcon>
+                        <PaymentsIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Payments" />
+                </ListItemButton>
+                <ListItemButton
+                    onClick={() => navigateTo('/user')}
+                    selected={path === '/user'}
+                >
+                    <ListItemIcon>
+                        <PeopleIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Customers" />
+                </ListItemButton>
+                <ListItemButton
+                    onClick={() => navigateTo('/storage-unit')}
+                    selected={path === '/storage-unit'}
+                >
+                    <ListItemIcon>
+                        <DomainIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Storage Units" />
+                </ListItemButton>
+                <ListItemButton
+                    onClick={() => navigateTo('/settings')}
+                    selected={path === '/settings'}
+                >
+                    <ListItemIcon>
+                        <SettingsIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Settings" />
+                </ListItemButton>
+            </List>
+
+            <div>
+                <Divider />
+                <List>
+                    <ListItemButton>
+                        <ListItemIcon><LogoutIcon /></ListItemIcon>
+                        <ListItemText primary="Logout" />
+                    </ListItemButton>
+                </List>
+            </div>
+
+        </div>
+    )
+};
